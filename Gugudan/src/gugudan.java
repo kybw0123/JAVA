f1b9956dde6ff720ad9f4a6c2e038abe98c1f3fe
YongBae Kim <73388879+kybw0123@users.noreply.github.com>
2020-12-23 20:41:46 +0900

    JAVA Gugudan
    Gugudan add and update

---
+++ Gugudan/src/gugudan.java
@@ -0,0 +1,91 @@
+//import java.util.Scanner;
+public class gugudan {
+	public static void main(String[] args) {
+//		//2단
+//		System.out.println("2단");
+//		System.out.println(2*1);
+//		System.out.println(2*2);
+//		System.out.println(2*3);
+//		System.out.println(2*4);
+//		System.out.println(2*5);
+//		System.out.println(2*6);
+//		System.out.println(2*7);
+//		System.out.println(2*8);
+//		System.out.println(2*9);
+//		
+//		//3단 
+//		System.out.println("3단");
+//		System.out.println(3*1);
+//		System.out.println(3*2);
+//		System.out.println(3*3);
+//		System.out.println(3*4);
+//		System.out.println(3*5);
+//		System.out.println(3*6);
+//		System.out.println(3*7);
+//		System.out.println(3*8);
+//		System.out.println(3*9);
+//		
+//		
+//		//4단
+//		System.out.println("4단");
+//		float result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+		
+//		//숫자 입력 받고 구구단 결과 노출
+//	    System.out.println("구구단 중 출력할 단은?");
+//	    Scanner scanner2 = new Scanner(System.in);
+//	    int number = scanner2.nextInt();
+//	    System.out.println("Number : " + number);
+//	    
+//	    double result2 = number * 1;
+//	    System.out.println(result2);
+//	    result2 = number * 2;
+//	    System.out.println(result2);
+//	    result2 = number * 3;
+//	    System.out.println(result2);
+//	    result2 = number * 4;
+//	    System.out.println(result2);
+//	    result2 = number * 5;
+//	    System.out.println(result2);
+//	    result2 = number * 6;
+//	    System.out.println(result2);
+//	    result2 = number * 7;
+//	    System.out.println(result2);
+//	    result2 = number * 8;
+//	    System.out.println(result2);
+//	    result2 = number * 9;
+//	    System.out.println(result2);
+	    
+//	    
+//	    // while 반복문을 사용하여 6단 만들기.
+//	    int i = 1;
+//	    while (i<10) {
+//	    	System.out.println("6 x " + i + " : " + 6*i);
+//	    	i = i+1;		
+//	    }
+		
+	    // for 반복문을 이용하여 7단 만들기.
+	    for(int j = 1; j<10; j++) {
+	    	System.out.println("7 x " + j + " : " + 7*j);
+	    }
+		
+	}
+	
+
+}
